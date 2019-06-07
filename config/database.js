74948c8201e6b06ed607d03ac876161d64d2f8f2
Zach Smith <zsmith126028@gmail.com>
2019-06-06 21:09:40 -0500

    add pool to database config

--- config/database.js
+++ config/database.js
@@ -2,7 +2,14 @@ const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize("baseballapi", "zach", process.env.PASSWORD, {
   host: "localhost",
-  dialect: "mysql"
+  dialect: "mysql",
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 });
 
 module.exports = sequelize;
