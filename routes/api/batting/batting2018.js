c9aec0cad98dc75b632e6aef26780b598e64182c
Zach Smith <zsmith126028@gmail.com>
2019-06-06 21:58:10 -0500

    add batting2018 rout

---
+++ routes/api/batting/batting2018.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../../../config/database");
+const Batting2018 = require("../../../models/Batting2018");
+const Sequelize = require("sequelize");
+
+router.get("/", (req, res) => {
+  Batting2018.findAll()
+    .sort({ war: -1 })
+    .then(batters => res.json(batters));
+});
+
+module.exports = router;
