500b0ecaae0f3ea29a2a9cb702525cccfd3b9d5d
Zach Smith <zsmith126028@gmail.com>
2019-06-07 17:07:42 -0500

    add route for 2018 relievers

---
+++ routes/api/pitchers/relievers/relievers2018.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../../../../config/database");
+const Relievers2018 = require("../../../../models/pitchers/starters/Relievers2018");
+const Sequelize = require("sequelize");
+
+router.get("/", (req, res) => {
+  Relievers2018.findAll().then(relievers => res.json(relievers));
+});
+
+module.exports = router;
