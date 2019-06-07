93fa791ab4ad4059cb43bf3ca6033931a234e767
Zach Smith <zsmith126028@gmail.com>
2019-06-07 17:04:23 -0500

    add route for 2018 starting pitching

---
+++ routes/api/pitchers/starters/starters2018.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../../../../config/database");
+const Starters2018 = require("../../../../models/pitchers/starters/Starters2018");
+const Sequelize = require("sequelize");
+
+router.get("/", (req, res) => {
+  Starters2018.findAll().then(starters => res.json(starters));
+});
+
+module.exports = router;
