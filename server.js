b681c867b6a09f79ea8b1712210244ead495f438
Zach Smith <zsmith126028@gmail.com>
2019-06-11 14:03:37 -0500

    rearrange routes to be chronological

--- server.js
+++ server.js
@@ -59,11 +59,11 @@ app.get("/", (req, res) => {
 });
 
 // Hitting Routes By Year
+app.use("/api/batting/2014", require("./routes/api/batting/batting2014"));
+app.use("/api/batting/2015", require("./routes/api/batting/batting2015"));
+app.use("/api/batting/2016", require("./routes/api/batting/batting2016"));
 app.use("/api/batting/2017", require("./routes/api/batting/batting2017"));
 app.use("/api/batting/2018", require("./routes/api/batting/batting2018"));
-app.use("/api/batting/2016", require("./routes/api/batting/batting2016"));
-app.use("/api/batting/2015", require("./routes/api/batting/batting2015"));
-app.use("/api/batting/2014", require("./routes/api/batting/batting2014"));
 
 // Starter Routes
 app.use(
