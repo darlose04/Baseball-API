b06ad4dbad956c8678ab3c0a48f146b0fbd47397
Zach Smith <zsmith126028@gmail.com>
2019-06-11 10:00:39 -0500

    add route for finding player by name. weird thing with a space in the url, need to figure that out

--- routes/api/batting/batting2014.js
+++ routes/api/batting/batting2014.js
@@ -3,9 +3,22 @@ const router = express.Router();
 const db = require("../../../config/database");
 const Batting2014 = require("../../../models/batters/Batting2014");
 const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
+// get all players in 2014 table
 router.get("/", (req, res) => {
   Batting2014.findAll().then(batters => res.json(batters));
+});
+
+// get player from 2014 by id
+router.get("/:name", (req, res) => {
+  // Batting2014.findByPk().then(batter => res.json(batter));
+
+  Batting2014.findOne({
+    where: {
+      name: req.params.name
+    }
+  }).then(batter => res.json(batter));
 });
 
 module.exports = router;
