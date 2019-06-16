0c1d92f1bea456abce81f7a01d6dcb220004f9f1
Zach Smith <zsmith126028@gmail.com>
2019-06-16 17:22:52 -0500

    add models for relievers for years 2014-17

---
+++ models/pitchers/relievers/Relievers2015.js
@@ -0,0 +1,96 @@
+const Sequelize = require("sequelize");
+const db = require("../../../config/database");
+
+const Relievers2015 = db.define(
+  "rp2015",
+  {
+    player_id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      allowNull: false
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    team: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    wins: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    losses: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    games: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    saves: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    innings_pitched: {
+      type: Sequelize.DECIMAL(4, 1),
+      allowNull: false
+    },
+    k_per_nine: {
+      type: Sequelize.DECIMAL(4, 2),
+      allowNull: false
+    },
+    bb_per_nine: {
+      type: Sequelize.DECIMAL(4, 2),
+      allowNull: false
+    },
+    hr_per_nine: {
+      type: Sequelize.DECIMAL(4, 2),
+      allowNull: false
+    },
+    babip: {
+      type: Sequelize.DECIMAL(4, 3),
+      allowNull: false
+    },
+    lob_perc: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    gb_perc: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    hr_per_fb: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    era: {
+      type: Sequelize.DECIMAL(4, 2),
+      allowNull: false
+    },
+    whip: {
+      type: Sequelize.DECIMAL(4, 2),
+      allowNull: false
+    },
+    fip: {
+      type: Sequelize.DECIMAL(4, 2),
+      allowNull: false
+    },
+    xfip: {
+      type: Sequelize.DECIMAL(4, 2),
+      allowNull: false
+    },
+    war: {
+      type: Sequelize.DECIMAL(3, 1),
+      allowNull: false
+    }
+  },
+  {
+    timestamps: false,
+    freezeTableName: true,
+    tableName: "rp2015"
+  }
+);
+
+module.exports = Relievers2015;
