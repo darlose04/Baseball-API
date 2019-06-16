516876a1e4440914e967babf12b521c0551dcdcc
Zach Smith <zsmith126028@gmail.com>
2019-06-16 15:32:55 -0500

    change db host to amazon rds endpoint, change db name as well

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("baseballapi", "zach", process.env.PASSWORD, {
-  host: "localhost",
+const sequelize = new Sequelize("baseballstats", "zach", process.env.PASSWORD, {
+  host: "baseballapi.cil0vkufrc31.us-east-1.rds.amazonaws.com",
   dialect: "mysql",
 
   pool: {
