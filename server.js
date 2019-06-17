13127c73f957c7c2b3a45d8787b1910690e101f3
Zach Smith <zsmith126028@gmail.com>
2019-06-17 14:31:12 -0500

    remove commented code that was no longer necessary

--- server.js
+++ server.js
@@ -18,39 +18,6 @@ db.authenticate()
 // db.connect();
 const app = express();
 
-// app.get("/batting2018", (req, res) => {
-//   const sql = `SELECT
-//     batting2018.name,
-//     batting2018.team,
-//     batting2018.games,
-//     batting2018.plate_appearances,
-//     batting2018.homeruns,
-//     batting2018.runs,
-//     batting2018.rbi,
-//     batting2018.sb,
-//     batting2018.bb_perc,
-//     batting2018.k_perc,
-//     batting2018.iso,
-//     batting2018.babip,
-//     batting2018.avg,
-//     batting2018.obp,
-//     batting2018.slg,
-//     batting2018.ops,
-//     batting2018.wOBA,
-//     batting2018.wRC_plus,
-//     batting2018.bsr,
-//     batting2018.off,
-//     batting2018.def,
-//     batting2018.war
-//     FROM batting2018
-//     ORDER BY war desc`;
-
-//   db.query(sql, (err, result) => {
-//     if (err) throw err;
-//     res.send(result);
-//   });
-// });
-
 app.set("view engine", "ejs");
 
 // Landing Page
