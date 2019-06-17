b52058e92e9f011a71393767b9197cdb945c1fa3
Zach Smith <zsmith126028@gmail.com>
2019-06-17 13:33:44 -0500

    create route to retrieve all batters

---
+++ routes/api/batting/allBatters.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../../../config/database");
+const AllBatters = require("../../../models/batters/AllBatters");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
+
+// get all players
+router.get("/", (req, res) => {
+  AllBatters.findAll({
+    order: [["team", "DESC"]]
+  }).then(batters => res.json(batters));
+});
