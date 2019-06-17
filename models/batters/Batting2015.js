c5b21bf76febfb6fa18adf6c5168ff91a03fc2b6
Zach Smith <zsmith126028@gmail.com>
2019-06-17 13:24:29 -0500

    add year column with different data value (DATEONLY)

--- models/batters/Batting2015.js
+++ models/batters/Batting2015.js
@@ -96,6 +96,10 @@ const Batting2015 = db.define(
     war: {
       type: Sequelize.DECIMAL(4, 1),
       allowNull: false
+    },
+    year: {
+      type: Sequelize.DATEONLY,
+      allowNull: false
     }
   },
   {
