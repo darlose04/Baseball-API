d32e2e95eef61d58884f9a2d570b074253e96e1e
Zach Smith <zsmith126028@gmail.com>
2019-06-17 19:07:24 -0500

    change routes to use the batters table from the db. This will allow me to delete most of the model files and only use the singular table with all the data

--- routes/api/batting/batting2014.js
+++ routes/api/batting/batting2014.js
@@ -1,21 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../../../config/database");
-const Batting2014 = require("../../../models/batters/Batting2014");
+const AllBatters = require("../../../models/batters/AllBatters");
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 
-// get all players in 2014 table
+// get all players from 2014
 router.get("/", (req, res) => {
-  Batting2014.findAll({
-    order: [["war", "DESC"]]
+  AllBatters.findAll({
+    where: {
+      year: 2014
+    }
+    // order: [["war", "DESC"]]
   }).then(batters => res.json(batters));
 });
 
 // get player from 2014 by name
 router.get("/players/:name", (req, res) => {
-  Batting2014.findOne({
+  AllBatters.findOne({
     where: {
+      year: 2014,
       name: req.params.name
     }
   }).then(batter => res.json(batter));
@@ -23,8 +27,9 @@ router.get("/players/:name", (req, res) => {
 
 // get team from 2014
 router.get("/teams/:teamName", (req, res) => {
-  Batting2014.findAll({
+  AllBatters.findAll({
     where: {
+      year: 2014,
       team: req.params.teamName
     }
   }).then(team => res.json(team));
