0295b3939e7d5d7635e3e98f52b8cd938b169646
Zach Smith <zsmith126028@gmail.com>
2019-06-17 20:21:45 -0500

    change link to model

--- routes/api/batting.js
+++ routes/api/batting.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../../config/database");
-const AllBatters = require("../../models/batters/AllBatters");
+const AllBatters = require("../../models/AllBatters");
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 
