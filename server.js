835608d1da02e0f5d0149a5c8d3db183df2db525
Zach Smith <zsmith126028@gmail.com>
2019-07-02 15:19:25 -0500

    add CORS

--- server.js
+++ server.js
@@ -20,6 +20,15 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // Landing Page
 app.get("/", (req, res) => {
   res.render("landing");
