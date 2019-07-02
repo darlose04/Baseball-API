e152609a3bde833be3548c6eff58066e492e4302
Zach Smith <zsmith126028@gmail.com>
2019-07-02 16:06:17 -0500

    edit CORS acces

--- server.js
+++ server.js
@@ -18,21 +18,19 @@ db.authenticate()
 // db.connect();
 const app = express();
 
+let allowCrossDomain = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Methods", "GET");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+};
+
+app.use(allowCrossDomain);
+
 app.set("view engine", "ejs");
-
-// let allowCrossDomain = function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// };
-
-// app.configure(function() {
-//   app.use(allowCrossDomain);
-// });
 
 // Landing Page
 app.get("/", (req, res) => {
