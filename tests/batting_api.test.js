45e41e5585fb0091eafbb553c933f998b52c7a4f
Zach Smith <zsmith126028@gmail.com>
2019-12-12 11:59:23 -0600

    add tests to check that batter stats are returned as json

--- tests/batting_api.test.js
+++ tests/batting_api.test.js
@@ -11,6 +11,34 @@ describe("testing routes that return hitting statistics", () => {
       .expect(200)
       .expect("Content-Type", /application\/json/);
   });
+
+  test("Individual batter statistics from each year are returned as json", async () => {
+    await api
+      .get("/api/batting/players/Mike Trout")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("Getting batters by year returns yearly stats as json", async () => {
+    await api
+      .get("/api/batting/2014")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("Get individual batter stats by year as json", async () => {
+    await api
+      .get("/api/batting/2016/players/Kris Bryant")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("Getting batter stats by year, by team returned as json", async () => {
+    await api
+      .get("/api/batting/2017/teams/Cubs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
 });
 
 afterAll(() => {
